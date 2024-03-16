d78c794a1960286bf85751d5d8dd6c4ddc0c165e
Ivan Podymov <p0dymovivan@yandex.ru>
2024-03-16 12:03:39 +0300

    added PostListView

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage,\
-                                  PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 
-def post_list(request):
-    post_list = Post.published.all()
-    # Pagination with 3 posts per page
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # If page_number is not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page_number is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
-    return render(request,
-                 'blog/post/list.html',
-                 {'posts': posts})
-
+class PosListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
 
 
 def post_detail(request, year, month, day, post):
